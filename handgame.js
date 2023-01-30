0f845923ef3a7584735e6efa36091f7058c68a0e
Akilou <akilou.issifou1999@gmail.com>
2023-01-30 17:53:29 -0500

    i finally figured out how to randomise my variables using chatgpt the code is randomItem =game[Math.floor(Math.random()*game.length)]; game is an array let game=[rock,paper,siccors]; if you do console.log(randomItem) it will be randomized now im trying to get it to fit into a function.

--- handgame.js
+++ handgame.js
@@ -1,3 +1,10 @@
-let rock=(`rock`)
-let paper=(`paper`)
-let scisser=(`scisser`)
+
+
+
+//console.log(randomItem);
+function getcomputerchoice(){
+    let game=[`rock`,`paper`,`siccors`];
+    return randomItem =game[Math.floor(Math.random()*game.length)]; 
+}
+let comp=getcomputerchoice();
+console.log
