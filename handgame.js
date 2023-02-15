94c9e1b601c6b54f08e2562b871990d27ca6eb7a
Akilou <akilou.issifou1999@gmail.com>
2023-02-14 19:53:11 -0500

    this is how you properly make a rock paper scissor game try to make it by yourself next time only taking a sneek peek for referance its okay to look your not dumb are slow for it

--- handgame.js
+++ handgame.js
@@ -1,31 +1,61 @@
+//console.log(randomItem);
 
+let p=0;
+let c=0;
 
-for(let i=0;i<5;i++){
-  function playRound(playerSelection, computerSelection) {
-    // your code here!
+function getcomputerchoice(){
+    let game=[`rock`,`paper`,`scissors`];
+    const rando=game[Math.floor(Math.random()*game.length)];
+    return rando;};
+
+    for(i=0;i<=5;i++){ 
+    let player=prompt(`round${i+1}:what do you choose(rock,paper,scissors?`)
+    let comp=getcomputerchoice();
   
-    if (playerSelection === computerSelection) {
-      return `its a tie`;
-    } else if (playerSelection === `rock` && computerSelection === `scissor`) {
-      return `you win rock beats scissors`
-    } else if (playerSelection === `paper` && computerSelection === `rock`) {
-      return `you win paper beats rock`
-    } else if (playerSelection === `scissor` && computerSelection === `paper`) {
-      return `you win scissors cut paper`
+
+    if(player===`rock`&&comp===`scissors`){
+        alert(`you win rock beats scissors`)
+        p++
     }
-  }
+    else if(player===`scissors`&&comp===`rock`){
+        alert(`you lose rock beats scissors`)
+        c++
+    }
+    else if(player===`paper`&&comp===`rock`){
+        alert(`you win paper beats rock`)
+        p++
+    }
+    else if(player===`rock`&&comp===`paper`){
+        alert(`you lose paper beats scissors`)
+        c++
+    }
+    else if(player===`scissors`&&comp===`paper`){
+        alert(`you win scissors beats rock`)
+        p++
+    }
+    else if(player===`paper`&&comp===`scissors`){
+        alert(`you lose scissors beats paper`)
+           c++
+    }
+    else if(player===`rock`&&comp===`rock`){
+        p++ 
+        c++
+        alert(`its a tie you both chose rock`)
+        
+    }
+        else if(player===`paper`&&comp===`paper`){
+            alert(`its a tie you both chose paper`)
+            p++ 
+            c++
+        }
+        else if(player===`scissors`&&comp===`scissors`){
+            alert(`its a tie you both chose scissors`)
+            p++ 
+            c++        
+        }
+
+        console.log(`Player wins: ${p} Computer wins: ${c}`);
   
-  function getComputerChoice() {
-    let g = [`rock`, `paper`, `scissor`]
-    return g[Math.floor(Math.random() * g.length)];
-  
+        
   }
-  
-  function playerchoice() {
-    let p=[`rock`,`paper`,`scissor`]
-    return p[Math.floor(Math.random()*p.length)];
-  }
-  const playerSelection =playerchoice();
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-}
+
