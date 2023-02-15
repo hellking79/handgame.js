a463251e624d92547f3d0bcfbe4c538086c5cf13
Akilou <akilou.issifou1999@gmail.com>
2023-02-15 16:25:58 -0500

    added some commits

--- handgame.js
+++ handgame.js
@@ -3,16 +3,19 @@
 let p=0;
 let c=0;
 
+//its a function that let the computer to choose
+//rock,paper,scissors at randome
+
 function getcomputerchoice(){
     let game=[`rock`,`paper`,`scissors`];
     const rando=game[Math.floor(Math.random()*game.length)];
     return rando;};
-
+// a funcrion that plays 5 rounds and keep track of the rounds  and how much
+// you wone and the computer wone
     for(i=0;i<=5;i++){ 
     let player=prompt(`round${i+1}:what do you choose(rock,paper,scissors?`)
     let comp=getcomputerchoice();
-  
-
+   
     if(player===`rock`&&comp===`scissors`){
         alert(`you win rock beats scissors`)
         p++
